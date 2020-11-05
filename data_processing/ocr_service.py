028837c7c97c08d166231e5ac5c0b1f0ae4ffebc
Amine Kammah <kammahamine@gmail.com>
2020-11-05 14:01:32 +0100

    Refactor kafka producer and consumer in a kafka_agent

--- data_processing/ocr_service.py
+++ data_processing/ocr_service.py
@@ -1,6 +1,6 @@
 from pyspark import SparkContext
 
-from messaging_agent.kafka_consumer import Consumer
+from messaging_agent.kafka_agent import KafkaAgent
 
 from data_processing.optical_character_recognizer import OpticalCharacterRecognizer
 
@@ -8,32 +8,35 @@ from data_processing.optical_character_recognizer import OpticalCharacterRecogni
 class OCRService:
     def __init__(self, kafka_server: str, kafka_topic: str, distribution_threshold: int = 50):
         self.distribution_threshold = distribution_threshold
-        self.kafka_server = kafka_server
+        self.kafka_agent = KafkaAgent(kafka_server)
         self.kafka_topic = kafka_topic
 
-    def distributed_extract(self, images):
+
+    @staticmethod
+    def _distributed_extract(images):
         sc = SparkContext("local", "first app")
         sc.addPyFile('/ensimag-sdtd/data_processing/optical_character_recognizer.py')
         from data_processing.optical_character_recognizer import OpticalCharacterRecognizer
 
         return sc.parallelize(images).map(lambda image: OpticalCharacterRecognizer.extract(image)).collect()
 
-    def parallelized_extract(self, images) -> List:
-        return list(images.map(lambda image: OpticalCharacterRecognizer.extract(image)))
+    @staticmethod
+    def _parallelized_extract(images) -> List:
+        return list(map(lambda image: OpticalCharacterRecognizer.extract(image), images))
 
     def start(self) -> None:
-        kafka_consumer = Consumer(self.kafka_server, self.kafka_topic)
 
         # TODO: Add exception handling
-        for batch in kafka_consumer.batch_consumer():
-            images = self.prep_batch(batch)
+        for batch in self.kafka_agent.batch_consumer(self.kafka_topic):
+            images = self._prep_batch(batch)
 
             if len(batch) > self.distribution_threshold:
-                extracted_text = self.distributed_extract(images)
+                extracted_text = self._distributed_extract(images)
             else:
-                extracted_text = self.parallelized_extract(images)
+                extracted_text = self._parallelized_extract(images)
 
             print(extracted_text)
 
-    def prep_batch(self, batch) -> List:
-        pass
+    @staticmethod
+    def _prep_batch(batch) -> List:
+        return batch
---
+++ messaging_agent/kafka_agent.py
@@ -0,0 +1,40 @@
+import os
+from typing import Generator, List
+import random
+
+from kafka import KafkaProducer, KafkaAdminClient, KafkaConsumer
+from kafka.admin import NewTopic
+
+
+class KafkaAgent:
+    def __init__(self, kafka_server: str) -> None:
+        self.kafka_server = kafka_server
+        self.kafka_producer = KafkaProducer(bootstrap_servers=self.kafka_server)
+
+    def create_topic(self, kafka_topic: str):
+        admin = KafkaAdminClient(bootstrap_servers=self.kafka_server)
+
+        topic = NewTopic(name=kafka_topic,
+                         num_partitions=1,
+                         replication_factor=1)
+        admin.create_topics([topic])
+
+    def batch_consumer(self, kafka_topic: str, batch_size: int = 100, timeout_ms: int = 1_000) -> Generator[List]:
+        kafka_consumer = KafkaConsumer(bootstrap_servers=self.kafka_server, consumer_timeout_ms=timeout_ms)
+        kafka_consumer.subscribe([kafka_topic])
+
+        while True:
+            images = [event for _, event in zip(range(batch_size), kafka_consumer)]
+            yield images
+
+    def produce(self, kafka_topic: str, data: List) -> None:
+
+        for value in data:
+            self.kafka_producer.send(kafka_topic, value=value)
+
+    def random_producer(self, kafka_topic: str, k: int) -> None:
+        test_data_path = "test_data/how_to_win_argments/"
+        dateset = os.listdir(test_data_path)
+
+        images = random.choices(dateset, k=k)
+        self.produce(kafka_topic, images)
