e6c64c585355207890bcd8e0a34eab3bb713cc68
Naima <naima12amalou@gmail.com>
2020-11-08 18:31:29 +0100

    delete the first implementation of EC2 instances

--- terraform/ec2.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_key_pair" "admin" {
-   key_name   = "admin"
-   public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
- }
- resource "aws_instance" "server1" {
-   ami           = "ami-0739f8cdb239fe9ae"
-   instance_type = "t2.medium"
-   key_name      = "admin"
- }
