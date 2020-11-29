e09251169bc3079b47ae80120402cf1662192d47
Naima <naima12amalou@gmail.com>
2020-11-30 00:12:30 +0100

    modification de run_kubeadm

--- run_kubeadm.sh
+++ run_kubeadm.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 chmod +x run_app.sh
 chmod +x destroy_app.sh
