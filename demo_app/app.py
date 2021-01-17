dff5cacde5cd6ea10c28b33d9efb44aa042934a5
root <root@ip-10-0-0-137.ec2.internal>
2021-01-17 18:55:29 +0000

    debug

--- demo_app/app.py
+++ demo_app/app.py
@@ -43,15 +43,15 @@ def update_extracted_text():
     sorted_by_time = sorted(timestamped_text, key=lambda element: element[0])
     print(timestamped_text)
     extracted_text = str(timestamped_text) + '<table class="table"><thead><tr><th scope="col">Image</th><th scope="col">Extracted Text</th></tr></thead><tbody>'
-    for event in sorted_by_time:
+    # for event in sorted_by_time:
 
-        value = json.loads(event[1].decode("utf-8"))
+    #     value = json.loads(event[1].decode("utf-8"))
 
-        text, image = value["text"], value["image"]
-        if len(text) > 500:
-            text = text[:500] + "..."
+    #     text, image = value["text"], value["image"]
+    #     if len(text) > 500:
+    #         text = text[:500] + "..."
 
-        extracted_text += f'<tr><th scope="row">{image}</th><td>{text}</td></tr>'
+    #     extracted_text += f'<tr><th scope="row">{image}</th><td>{text}</td></tr>'
 
     extracted_text += "</tbody></table>"
     global output, total_processed
