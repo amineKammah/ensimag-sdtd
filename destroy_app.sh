be2997fc7e117c6e1e368cc1d3a189eef7e6bf80
root <root@ip-10-0-0-137.ec2.internal>
2021-01-23 18:21:06 +0000

    Make deployments more fault tolerant

--- destroy_app.sh
+++ destroy_app.sh
@@ -3,4 +3,5 @@ kubectl delete --all jobs --force
 kubectl delete --all deployments --force
 kubectl delete --all pods --force
 kubectl delete --all services --force
-rm adress_ip.txt
+kubectl delete --all pvc --force
+rm adress_ip
